6cf2194972f6515fbe315c525a88f22b019f1bce
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2023-10-16 17:44:22 -0400

    32112

--- DOM.js
+++ DOM.js
@@ -1,14 +1,15 @@
 const DOMSelectors = {
     form: document.querySelector("form"),
     firstName: document.querySelector(".first-name"),
-    h2s: document.querySelector("h2")
+    lastName: document.querySelector(".last-name"),
+    age: document.querySelector(".age"),
+    h2s: document.querySelectorAll("h2")
 };
 
 DOMSelectors.form.addEventListener("submit", function(event){
     event.preventDefault();
     console.log(DOMSelectors.firstName.value) 
-    DOMSelectors.h2.forEach(
-        (el) => { el.textContent = DOMSelectors.firstName.value
-        
-    });
+    DOMSelectors.h2s.forEach(
+        (el) => { el.textContent = DOMSelectors.firstName.value }
+    );
 });
