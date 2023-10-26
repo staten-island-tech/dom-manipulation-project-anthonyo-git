a3fa9dce7722c236b23bf75b5ee7640921d6e0b8
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2023-10-25 22:30:32 -0400

    52

--- DOM.js
+++ DOM.js
@@ -1,20 +1,32 @@
 const DOMSelectors = {
   form: document.querySelector("form"),
-  firstName: document.querySelector(".first-name"),
-  lastName: document.querySelector(".last-name"),
+  Header: document.querySelector(".head"),
+  Text: document.querySelector(".texts"),
   Image: document.querySelector(".image"),
   h2s: document.querySelectorAll("h2"),
-  imgs: document.querySelectorALL("img")
+  imgs: document.querySelectorAll("img")
 };
 
 DOMSelectors.form.addEventListener("submit", function (event) {
   event.preventDefault();
+
+let card = {
+  Header: DOMSelectors.Header.value,
+  Text: DOMSelectors.Text.value,
+  image: DOMSelectors.Image.value
+};
+
+  let h2 = document.createElement("h2");
+  let img = document.createElement("img");
+
+}
+
+/*
   console.log(DOMSelectors.firstName.value);
   console.log(DOMSelectors.Image.value);
   DOMSelectors.h2s.forEach((el) => {
     el.textContent = DOMSelectors.firstName.value;
   });
-  DOMSelectors.imgs.forEach((el) => {
-    el.image = DOMSelectors.Image.value;
-  })
 });
+ */
+
