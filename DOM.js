3b6c2ca763dc29d9f9db6519592e7a308d83c576
anthonyo-git <124058513+anthonyo-git@users.noreply.github.com>
2024-01-11 21:55:08 -0500

    s

--- DOM.js
+++ DOM.js
@@ -1,24 +1,27 @@
-const DOMSelectors = {
-  form: document.querySelector("#form"),
+const DOMSelectors = { // -Create an object called “DOMSelectors” to hold your DOM Selectors
+  form: document.querySelector("#form"), 
   Header: document.querySelector(".head"),
   Text: document.querySelector(".texts"),
   Image: document.querySelector(".image"),
   h2s: document.querySelectorAll("h2"),
   imgs: document.querySelectorAll("img")
 };
+function afterSubmit(event) { // -Create a function that creates an object and calls the following functions
+  event.preventDefault();
 
-function creatcard() {
   const card = {
     Header: DOMSelectors.Header.value,
     Text: DOMSelectors.Text.value,
     Image: DOMSelectors.Image.value
   } 
-  return card;
-};
+  insert(card);
+}
+
+DOMSelectors.form.addEventListener("submit", afterSubmit);
+
   
-function insert(card) {
-  document.querySelector(".Flexbox-container").insertAdjacentHTML(
-    "beforeend", 
+function insert(card) { // -Create a function that injects the newly created object into the DOM
+    const newCard = 
     `<div class="cell">
         <div class="text-container">
           <h2>${card.Header}</h2>
@@ -26,15 +29,27 @@ function insert(card) {
         </div>
         <img src="${card.Image}">
         <button class="remove-button">Remove</button>
-      </div>`
-  );
+      </div>`;
 
-    const newCard = document.querySelector(".Flexbox-container").lastElementChild;
-  newCard.querySelector(".remove-button").addEventListener("click", function() {
-    newCard.remove();
+  document.querySelector(".Flexbox-container").insertAdjacentHTML('beforeend', newCard);
+
+  let removeButtons = document.querySelectorAll(".remove-button");// -Create a function to remove an object after they have been created
+  removeButtons.forEach((btn) => {
+   btn.addEventListener("click", function () {
+     this.parentElement.remove();
+   });
   });
+
+  clearFields();
  }
    
+
+ function clearFields() { // -Create a function that clears the input fields after injecting the object
+  DOMSelectors.Header.value = "";
+  DOMSelectors.Text.value = "";
+  DOMSelectors.Image.value = "";
+ }
+
 /*
 
   console.log(DOMSelectors.firstName.value);
@@ -45,10 +60,4 @@ function insert(card) {
 });
  */
 
-
-DOMSelectors.form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  const card = creatcard();
-  insert(card);
-});;
 
